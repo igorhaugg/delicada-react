3f915872875aea341e3f39c891b6772080fc9f84
Igor Haugg <igor-haugg@hotmail.com>
2018-05-04 12:01:26 -0300

    details page

--- src/components/site/Footer.js
+++ src/components/site/Footer.js
@@ -11,61 +11,84 @@ const Footer = props => {
     }
   };
   return (
-    <footer className="footer wrapper">
-      <div className="footer__links">
-        <span className="footer__title">Mais Informações</span>
-        <ul className="footer__list">
-          <li>
-            <Link to="/" className="color-link">
-              <i className="fas fa-angle-right" /> Início
-            </Link>
-          </li>
-          <li className="color-link">
-            <i className="fas fa-angle-right" /> Formas de Pagamento
-            <ul className="sub-link">
-              <li className="color-link">- Cartão de Crédito</li>
-              <li className="color-link">- Dinheiro</li>
-            </ul>
-          </li>
-          <li>
-            <Link to="/about" className="color-link">
-              <i className="fas fa-angle-right" /> Sobre
-            </Link>
-          </li>
-          <li>
-            <Link to="/login" className="color-link">
-              <i className="fas fa-angle-right" /> Login
-            </Link>
-          </li>
-        </ul>
-      </div>
-      <div className="footer__products">
-        <span className="footer__title">Produtos</span>
-        <ul className="footer__list">
-          {props.categories.map(category => (
-            <li key={category.id} onClick={() => onCategoryChange(category.id)}>
-              <Link to="/products" className="color-link">
-                <i className="fas fa-angle-right" />
-                &nbsp;
-                {category.name}
+    <footer>
+      <div className="footer wrapper">
+        <div className="footer__links">
+          <span className="footer__title">Mais Informações</span>
+          <ul className="footer__list">
+            <li>
+              <Link to="/" className="color-link">
+                <i className="fas fa-angle-right" /> Início
               </Link>
             </li>
-          ))}
-        </ul>
+            <li className="color-link">
+              <i className="fas fa-angle-right" /> Formas de Pagamento
+              <ul className="sub-link">
+                <li className="color-link">- Cartão de Crédito</li>
+                <li className="color-link">- Dinheiro</li>
+              </ul>
+            </li>
+            <li>
+              <Link to="/about" className="color-link">
+                <i className="fas fa-angle-right" /> Sobre
+              </Link>
+            </li>
+            <li>
+              <Link to="/login" className="color-link">
+                <i className="fas fa-angle-right" /> Login
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="footer__products">
+          <span className="footer__title">Produtos</span>
+          <ul className="footer__list">
+            {props.categories.map(category => (
+              <li
+                key={category.id}
+                onClick={() => onCategoryChange(category.id)}
+              >
+                <Link to="/products" className="color-link">
+                  <i className="fas fa-angle-right" />
+                  &nbsp;
+                  {category.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="footer__contact">
+          <span className="footer__title">Contato</span>
+          <ul className="footer__list">
+            <li className="color-link">
+              <i className="fab fa-whatsapp" /> 959465121
+            </li>
+            <li className="color-link">
+              <i className="fas fa-envelope" /> email@email.com
+            </li>
+            <li className="color-link">
+              <i className="fas fa-location-arrow" /> Ijuí - RS
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="footer__contact">
-        <span className="footer__title">Contato</span>
-        <ul className="footer__list">
-          <li className="color-link">
-            <i className="fab fa-whatsapp" /> 959465121
-          </li>
-          <li className="color-link">
-            <i className="fas fa-envelope" /> email@email.com
-          </li>
-          <li className="color-link">
-            <i className="fas fa-location-arrow" /> Ijuí - RS
-          </li>
-        </ul>
+
+      <div className="copyright wrapper">
+        <div className="copyright__border">
+          <span className="color-link">Copyright 2018</span>
+          <span className="copyright__names">
+            <a href="https://igorhaugg.github.io/portfolio/" target="_blank">
+              Igor Haugg
+            </a>{' '}
+            <span className="color-link">e </span>
+            <a
+              href="https://marciadasilva.github.io/portfolio/"
+              target="_blank"
+            >
+              Márcia da Silva
+            </a>
+          </span>
+        </div>
       </div>
     </footer>
   );
